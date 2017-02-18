4c9a37320dd848101ea6f3dbb1c208f4d43cd020
Neely Fawaz <neelyfawaz@gmail.com>
2017-02-17 21:46:32 -0500

    Working on adding modal window

---
+++ webapp/js/plugins/modalWindow.js
@@ -0,0 +1,36 @@
+/**
+ * Created by neely on 1/30/17.
+ */
+
+(function($) {
+    'use strict';
+
+    var modal_window = $("#modal");
+    var modal_message = $("#modal-message");
+    var modal_header = $("#modal-header");
+
+    $.openModal = function (message, header) {
+        console.log(message);
+        modal_message.text(message);
+        modal_header.text(header);
+        modal_window.modal('show');
+    }
+
+    $.closeModal = function () {
+        modal_window.modal('hide');
+        modal_message.text(" ");
+        modal_header.text(" ");
+    }
+
+    $(".close-modal").click( function() {
+        $.closeModal();
+    });
+
+    $("body").on('click', ".modal-backdrop", function() {
+        $.closeModal();
+    });
+
+
+})(jQuery);
+
+$.openModal("testing 12345", "Error");
